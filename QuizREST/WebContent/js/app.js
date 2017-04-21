6da9b6e806cd032598c16c27c8262ee6bf998473
Shaun Dashjian <shaundashjian@yahoo.com>
2017-04-21 10:24:31 -0600

    Added full CRUD

--- QuizREST/WebContent/js/app.js
+++ QuizREST/WebContent/js/app.js
@@ -9,12 +9,74 @@ var loadData = function() {
         dataType: 'json'
     }).done(function(quizzes, status) {
 
-        buildTableAndCreateForm(quizzes);
+        listQuizzesAndShowCreateQuizButton(quizzes);
     }).fail(function(xhr, status, error) {
         $('#content').append('<p>An Error has Occured</p>');
     });
 }
 
+var listQuizzesAndShowCreateQuizButton = function(quizzes) {
+    $('#content').empty();
+    var $table = $('<table id="table" class="table table-bordered bordered">');
+
+    var $thead = $('<thead>');
+    $thead.append('<tr><th>Quiz Name</th><th class="fit">Action</th><th class="fit">Action</th><th class="fit">Action</th></tr>');
+    $table.append($thead);
+    var $tbody = $('<tbody>');
+    quizzes.forEach(function(quiz, index, array) {
+        $tbody.append('<tr><td >' + capitalizeFirstLetter(quiz.name) 
+        		+ '</td><td quizId="' + quiz.id + '"></td><td quizId="' + quiz.id 
+        		+ '"></td><td quizId="' + quiz.id + '"></td></tr>');
+    })
+    $table.append($tbody);
+    $('#content').append('<div class="container">');
+    $('.container').append($table);
+
+    $($table).wrap('<div class="col-md-6 col-md-offset-3 bordered" id="mainColumn"></div>');
+    $('#mainColumn').wrap('<div class="row"></div>');
+    $('#mainColumn').prepend("<h1 class=' backgrounded bordered'>It's Quiz Time!</h1>");
+
+    var $viewButtonCells = $('tr td:nth-child(2)');
+    $viewButtonCells.each(function() {
+
+        var $viewButton = $('<button  type="button" name="viewButton" class="btn btn-success">View</button>');
+        $viewButton.attr('id', $(this).attr("quizId"));
+        $(this).append($viewButton);
+        $viewButton.click(function() {
+            showQuiz($(this).attr('id'));
+        });        
+    });
+    
+    var $editButtonCells = $('tr td:nth-child(3)');
+    $editButtonCells.each(function() {
+
+        var $editButton = $('<button  type="button" name="editButton" class="btn btn-warning">Edit</button>');
+        $editButton.attr('id', $(this).attr("quizId"));
+        $(this).append($editButton);
+        $editButton.click(function() {
+            editQuiz($(this).attr('id'));
+        });        
+    });
+    
+    var $deleteButtonCells = $('tr td:nth-child(4)');
+    $deleteButtonCells.each(function() {
+
+        var $deleteButton = $('<button  type="button" name="deleteButton" class="btn btn-danger">Delete</button>');
+        $deleteButton.attr('id', $(this).attr("quizId"));
+        $(this).append($deleteButton);
+        $deleteButton.click(function() {
+            deleteQuiz($(this).attr('id'));
+        });        
+    });
+    
+    // Create Quiz button
+    var $createQuizButton = $('<button  type="button" id="showCreateQuizButton" class="btn btn-primary">Create Quiz</button>');
+    $('#mainColumn').append($createQuizButton);
+    $createQuizButton.click(function() {
+        showCreateQuizForm();
+    });   
+
+}
 
 var showQuiz = function(id) {
     cleanUpAndAddMainColumn();
@@ -27,6 +89,39 @@ var showQuiz = function(id) {
     }).fail(function(xhr, status, error) {
         $('#content').append('<p>An Error has Occured</p>');
     });
+
+}
+
+var editQuiz = function(id) {
+    cleanUpAndAddMainColumn();
+    $.ajax({
+        type: 'GET',
+        url: 'api/quizzes/' + id,
+        dataType: 'json'
+    }).done(function(quiz, status) {
+        showQuizDetailsToEdit(quiz);
+    }).fail(function(xhr, status, error) {
+        $('#content').append('<p>An Error has Occured</p>');
+    });
+
+}
+
+var deleteQuiz = function(id) {
+	var deleteQuiz = confirm("Are you sure you want to delete this quiz?");
+	if (deleteQuiz) {
+		
+		cleanUpAndAddMainColumn();
+		// DELETE quizzes/{id}
+		$.ajax({
+			type: 'DELETE',
+			url: 'api/quizzes/' + id,
+		}).done(function(quiz, status) {
+			loadData();
+		}).fail(function(xhr, status, error) {
+			$('#content').append('<p>An Error has Occured</p>');
+			addReturnButton();
+		});
+	}
 
 }
 
@@ -70,92 +165,112 @@ var showQuizDetails = function(quiz) {
 
 }
 
-var buildTableAndCreateForm = function(quizzes) {
-    $('#content').empty();
-    var $table = $('<table id="table" class="table table-bordered bordered">');
+var showQuizDetailsToEdit = function(quiz) {
+	
+	
+	// PUT quizzes/{id}
+	var $form = $('<form name="editQuizForm" id="editQuizForm">');
+	$form.append('<label for="name">Quiz Name: </label>');
+	$form.append('<input id="name" type="text" name="name"  size="12" value="'+quiz.name+'" required><br>');
+	
+	// edit button
+	var $editButton = $('<input id="edit" type="submit" name="editQuizButton" class="btn btn-warning" value="Submit">');
+	$form.append($editButton);
+//	$createButton.click(function(event) {
+	$form.submit(function(event) {
+		event.preventDefault();
+		if ($(editQuizForm.name).val()) {
+			
+			var Quiz = {
+					name: $(editQuizForm.name).val()
+			};
+			$.ajax({
+				type: "PUT",
+		        url: 'api/quizzes/'+quiz.id,
+				dataType: "json",
+				contentType: 'application/json', //setting the request headers content-type
+				data: JSON.stringify(Quiz) //the data being added to the request body
+			}).done(function(Quiz, status) {
+			    cleanUpAndAddMainColumn();
+				confirmQuizEditted(Quiz);
+			}).fail(function(xhr, status, error) {
+			    cleanUpAndAddMainColumn();
+				$('#mainColumn').append('<p>An Error has Occured</p>');
+				addReturnButton();
+			});
+		} else {
+			$form.prepend('<p>Please enter quiz name!</p>');
 
-    var $thead = $('<thead>');
-    $thead.append('<tr><th>Quiz Name</th><th class="fit">Action</th></tr>');
-    $table.append($thead);
-    var $tbody = $('<tbody>');
-    quizzes.forEach(function(quiz, index, array) {
-        $tbody.append('<tr><td >' + capitalizeFirstLetter(quiz.name) 
-        		+ '</td><td quizId="' + quiz.id + '"></td></tr>');
-    })
-    $table.append($tbody);
-    $('#content').append('<div class="container">');
-    $('.container').append($table);
-
-    $($table).wrap('<div class="col-md-6 col-md-offset-3 bordered" id="mainColumn"></div>');
-    $('#mainColumn').wrap('<div class="row"></div>');
-    $('#mainColumn').prepend("<h1 class=' backgrounded bordered'>It's Quiz Time!</h1>");
-
-    var $viewButtonCells = $('tr td:nth-child(2)');
-    $viewButtonCells.each(function() {
-
-        var $viewButton = $('<button  type="button" name="button" class="btn btn-primary">View</button>');
-        $viewButton.attr('id', $(this).attr("quizId"));
-        $(this).append($viewButton);
-        $viewButton.click(function() {
-            showQuiz($(this).attr('id'));
-        });        
-    });
-
-
-//    var $form = $('<form name="createForm" id="createForm">');
-//    $form.append('<input id="pokeId" type="number" name="pokeId"  size="12" min="1" placeholder="Pokemon ID"><br>');
-//    $form.append('<input id="name" type="text" name="name"  size="12" placeholder="Name"><br>');
-//    $form.append('<input id="height" type="number" name="height"  size="6" min="1" placeholder="Height"><br>');
-//    $form.append('<input id="weight" type="number" name="weight"  size="6" min="1" placeholder="Weight"><br>');
-//    $form.append('<input id="img" type="text" name="img"  size="60" placeholder="Image URL"><br>');
-//    $form.append('<textarea id="description" name="description"  cols="40" rows="4" placeholder="Description"></textarea><br>');
-//
-//    // create button
-//    var $createButton = $('<input id="create" type="submit" name="create" class="btn btn-primary" value="Create">');
-//    $form.append($createButton);
-//    $createButton.click(function(event) {
-//        event.preventDefault();
-//        var pokemon = {
-//            pokeId: $(createForm.pokeId).val(),
-//            name: $(createForm.name).val(),
-//            height: $(createForm.height).val(),
-//            height: $(createForm.weight).val(),
-//            img: $(createForm.img).val(),
-//            description: $(createForm.description).val()
-//        };
-//        $.ajax({
-//            type: "POST",
-//            url: "http://52.25.225.137:8080/pokemon/data/poke/",
-//            dataType: "json",
-//            contentType: 'application/json', //setting the request headers content-type
-//            data: JSON.stringify(pokemon) //the data being added to the request body
-//        }).done(function(pokemon, status) {
-//            confirmPokemonAdded(pokemon);
-//        }).fail(function(xhr, status, error) {
-//            $('#content').append('<p>An Error has Occured</p>');
-//        });
-//    });
-//
-//
-//    $('.container').append($form);
-//    $($form).wrap('<div class="col-md-6 col-md-offset-3 bordered form" id="formColumn"></div>');
-//    $('#formColumn').wrap('<div class="row"></div>');
-//    $('#formColumn').prepend("<h1 class=' backgrounded bordered'>Create New Pokemon!</h1>");
+		}
+	});
+	
+	
+	$('#mainColumn').append("<h1 class='backgrounded bordered'>Edit Quiz!</h1>");
+	$('#mainColumn').append($form);
+	
+	
 }
 
-var confirmPokemonAdded = function(pokemon) {
-    loadData();
-    // cleanUpAndAddMainColumn();
-    // buildTableAndCreateForm();
-    // $('#mainColumn').append('<h2>New Pokemon Added!</h2>');
-    // showPokemonDetails(pokemon);
+var showCreateQuizForm = function(){
+	$('#showCreateQuizButton').remove();
+	var $form = $('<form name="createQuizForm" id="createQuizForm">');
+	$form.append('<input id="name" type="text" name="name"  size="12" placeholder="Quiz Name" required><br>');
+	
+	// create button
+	var $createButton = $('<input id="create" type="submit" name="createQuizButton" class="btn btn-primary" value="Create">');
+	$form.append($createButton);
+//	$createButton.click(function(event) {
+	$form.submit(function(event) {
+		event.preventDefault();
+		if ($(createQuizForm.name).val()) {
+			
+			var Quiz = {
+					name: $(createQuizForm.name).val()
+			};
+			$.ajax({
+				type: "POST",
+		        url: 'api/quizzes',
+				dataType: "json",
+				contentType: 'application/json', //setting the request headers content-type
+				data: JSON.stringify(Quiz) //the data being added to the request body
+			}).done(function(Quiz, status) {
+				confirmQuizAdded(Quiz);
+			}).fail(function(xhr, status, error) {
+				$('#content').append('<p>An Error has Occured</p>');
+			});
+		} else {
+			$form.prepend('<p>Please enter quiz name!</p>');
+
+		}
+	});
+	
+	
+	$('.container').append($form);
+	$($form).wrap('<div class="col-md-6 col-md-offset-3 bordered form" id="formColumn"></div>');
+	$('#formColumn').wrap('<div class="row"></div>');
+	$('#formColumn').prepend("<h1 class='backgrounded bordered'>Create New Quiz!</h1>");
 }
 
-var confirmPokemonDeleted = function() {
+var confirmQuizAdded = function(Quiz) {
     loadData();
-
     // cleanUpAndAddMainColumn();
-    // $('#mainColumn').append('<h2>Pokemon Deleted!</h2>');
+    // listQuizzesAndShowCreateQuizButton();
+    // $('#mainColumn').append('<h2>New Quiz Added!</h2>');
+    // showQuizDetails(Quiz);
+}
+
+var confirmQuizEditted = function(Quiz) {
+    loadData();
+    // cleanUpAndAddMainColumn();
+    // listQuizzesAndShowCreateQuizButton();
+    // $('#mainColumn').append('<h2>New Quiz Added!</h2>');
+    // showQuizDetails(Quiz);
+}
+
+var confirmQuizDeleted = function() {
+    loadData();
+    // cleanUpAndAddMainColumn();
+    // $('#mainColumn').append('<h2>Quiz Deleted!</h2>');
     // addReturnButton();
 }
 
